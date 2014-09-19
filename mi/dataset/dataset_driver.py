2cba4589390682132868339f34644ab9c682d23e
Mark Worden <mark_c_worden@raytheon.com>
2014-09-19 09:43:17 -0400

    Update to dataset_driver for testing and inclusion of missing yml files.

--- mi/dataset/dataset_driver.py
+++ mi/dataset/dataset_driver.py
@@ -3,18 +3,26 @@ __author__ = 'wordenm'
 from mi.core.log import get_logger
 log = get_logger()
 
+
 # The following class is a stub class.  The real class is a Java class.
-# This script is expected to be used via JEP (Java Embedded Python)
+# This script is expected to be used via JEP (Java Embedded Python).
+# This stub class is needed for driver level testing without uFrame.
 class ParticleDataHandler(object):
 
     def __init__(self):
-        pass
+        self._samples = {}
+        self._failure = False
 
     def addParticleSample(self, sample_type, sample):
         log.debug("Sample type: %s, Sample data: %s", sample_type, sample)
+        if sample_type not in self._samples.keys():
+            self._samples[sample_type] = [sample]
+        else:
+            self._samples[sample_type].append(sample)
 
     def setParticleDataCaptureFailure(self):
         log.debug("Particle data capture failed")
+        self._failure = True
 
 
 class DataSetDriver:
