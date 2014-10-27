7f64c6e61e6518981803433db0543f7dbe45637c
Joe Padula <Joseph_J_Padula@raytheon.com>
2014-10-27 10:17:03 -0400

    Driver files for dosta_abcdjm_dcl ready for code review.
    Change-Id: Ic514294000e9e924a3fe020f25fd6b480c9a28e2

---
+++ mi/dataset/driver/dosta_abcdjm/dcl/dosta_abcdjm_dcl_recovered_driver.py
@@ -0,0 +1,59 @@
+# #
+# OOIPLACEHOLDER
+#
+# Copyright 2014 Raytheon Co.
+##
+
+__author__ = "Joe Padula"
+
+import os
+
+from mi.core.log import get_logger
+from mi.logging import config
+
+from mi.dataset.dataset_parser import DataSetDriverConfigKeys
+
+from mi.dataset.dataset_driver import DataSetDriver
+from mi.dataset.parser.dosta_abcdjm_dcl import DostaAbcdjmDclRecoveredParser
+
+
+class DostaAbcdjmDclRecoveredDriver:
+    def __init__(self, sourceFilePath, particleDataHdlrObj, parser_config):
+        self._sourceFilePath = sourceFilePath
+        self._particleDataHdlrObj = particleDataHdlrObj
+        self._parser_config = parser_config
+
+    def process(self):
+        log = get_logger()
+
+        with open(self._sourceFilePath, "r") as file_handle:
+            def exception_callback(exception):
+                log.debug("Exception: %s", exception)
+                self._particleDataHdlrObj.setParticleDataCaptureFailure()
+
+            parser = DostaAbcdjmDclRecoveredParser(
+                self._parser_config,
+                file_handle,
+                None,
+                lambda state, ingested: None,
+                lambda data: None,
+                exception_callback)
+
+            driver = DataSetDriver(parser, self._particleDataHdlrObj)
+
+            driver.processFileStream()
+            log.debug("DostaAbcdjmDclRecoveredDriver: processing file stream")
+        return self._particleDataHdlrObj
+
+
+def parse(basePythonCodePath, sourceFilePath, particleDataHdlrObj):
+    config.add_configuration(os.path.join(basePythonCodePath, 'res', 'config', 'mi-logging.yml'))
+
+    parser_config = {
+        DataSetDriverConfigKeys.PARTICLE_MODULE: 'mi.dataset.parser.dosta_abcdjm_dcl',
+        DataSetDriverConfigKeys.PARTICLE_CLASS: None
+    }
+
+    driver = DostaAbcdjmDclRecoveredDriver(sourceFilePath, particleDataHdlrObj, parser_config)
+
+    return driver.process()
