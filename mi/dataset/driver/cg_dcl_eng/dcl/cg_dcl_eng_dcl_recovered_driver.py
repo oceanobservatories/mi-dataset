7316e8b05fcdd90c2f1d749a2576ce1d2bd57956
Mark Worden <mark_c_worden@raytheon.com>
2014-11-05 15:47:22 -0500

    cg_dcl_eng_dcl delivery
    Change-Id: Id75509593fc47fef4110c200d8557c26f731220d

---
+++ mi/dataset/driver/cg_dcl_eng/dcl/cg_dcl_eng_dcl_recovered_driver.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+"""
+@package mi.dataset.driver.cg_dcl_eng.dcl
+@file mi-dataset/mi/dataset/driver/cg_dcl_eng/dcl/cg_dcl_eng_dcl_recovered_driver.py
+@author Mark Worden
+@brief Driver for the cg_dcl_eng_dcl instrument
+
+Release notes:
+
+Initial Release
+"""
+
+from mi.dataset.dataset_parser import DataSetDriverConfigKeys
+from mi.dataset.dataset_driver import SimpleDatasetDriver
+from mi.dataset.parser.cg_dcl_eng_dcl import CgDclEngDclParser, CgDclEngDclParticleClassTypes, \
+    CgDclEngDclMsgCountsRecoveredDataParticle, \
+    CgDclEngDclCpuUptimeRecoveredDataParticle, \
+    CgDclEngDclErrorRecoveredDataParticle, \
+    CgDclEngDclGpsRecoveredDataParticle, \
+    CgDclEngDclPpsRecoveredDataParticle, \
+    CgDclEngDclSupervRecoveredDataParticle, \
+    CgDclEngDclDlogMgrRecoveredDataParticle, \
+    CgDclEngDclDlogStatusRecoveredDataParticle, \
+    CgDclEngDclStatusRecoveredDataParticle, \
+    CgDclEngDclDlogAarmRecoveredDataParticle
+
+
+def parse(basePythonCodePath, sourceFilePath, particleDataHdlrObj):
+    """
+    This is the method called by Uframe
+    :param basePythonCodePath This is the file system location of mi-dataset
+    :param sourceFilePath This is the full path and filename of the file to be parsed
+    :param particleDataHdlrObj Java Object to consume the output of the parser
+    :return particleDataHdlrObj
+    """
+
+    with open(sourceFilePath, 'rb') as stream_handle:
+
+        driver = CgDclEngDclRecoveredDriver(basePythonCodePath, stream_handle, particleDataHdlrObj)
+        driver.processFileStream()
+
+    return particleDataHdlrObj
+
+
+class CgDclEngDclRecoveredDriver(SimpleDatasetDriver):
+    """
+    Derived flntu_x_mmp_cds driver class
+    All this needs to do is create a concrete _build_parser method
+    """
+
+    def _build_parser(self, stream_handle):
+
+        parser_config = {
+            DataSetDriverConfigKeys.PARTICLE_MODULE: 'mi.dataset.parser.cg_dcl_eng_dcl',
+            DataSetDriverConfigKeys.PARTICLE_CLASS: None,
+            DataSetDriverConfigKeys.PARTICLE_CLASSES_DICT: {
+                CgDclEngDclParticleClassTypes.MSG_COUNTS_PARTICLE_CLASS:
+                    CgDclEngDclMsgCountsRecoveredDataParticle,
+                CgDclEngDclParticleClassTypes.CPU_UPTIME_PARTICLE_CLASS:
+                    CgDclEngDclCpuUptimeRecoveredDataParticle,
+                CgDclEngDclParticleClassTypes.ERROR_PARTICLE_CLASS:
+                    CgDclEngDclErrorRecoveredDataParticle,
+                CgDclEngDclParticleClassTypes.GPS_PARTICLE_CLASS:
+                    CgDclEngDclGpsRecoveredDataParticle,
+                CgDclEngDclParticleClassTypes.PPS_PARTICLE_CLASS:
+                    CgDclEngDclPpsRecoveredDataParticle,
+                CgDclEngDclParticleClassTypes.SUPERV_PARTICLE_CLASS:
+                    CgDclEngDclSupervRecoveredDataParticle,
+                CgDclEngDclParticleClassTypes.DLOG_MGR_PARTICLE_CLASS:
+                    CgDclEngDclDlogMgrRecoveredDataParticle,
+                CgDclEngDclParticleClassTypes.DLOG_STATUS_PARTICLE_CLASS:
+                    CgDclEngDclDlogStatusRecoveredDataParticle,
+                CgDclEngDclParticleClassTypes.STATUS_PARTICLE_CLASS:
+                    CgDclEngDclStatusRecoveredDataParticle,
+                CgDclEngDclParticleClassTypes.DLOG_AARM_PARTICLE_CLASS:
+                    CgDclEngDclDlogAarmRecoveredDataParticle,
+            }
+
+        }
+
+        parser = CgDclEngDclParser(parser_config,
+                                   stream_handle,
+                                   self._exception_callback)
+
+        return parser
+
+
---
+++ mi/dataset/driver/cg_dcl_eng/dcl/test/test_cg_dcl_eng_dcl_telemetered_driver.py
@@ -0,0 +1,42 @@
+#!/home/mworden/uframes/ooi/uframe-1.0/python/bin/python
+
+__author__ = 'mworden'
+
+from mi.core.log import get_logger
+log = get_logger()
+
+from mi.idk.config import Config
+
+import unittest
+import os
+from mi.dataset.driver.cg_dcl_eng.dcl.cg_dcl_eng_dcl_telemetered_driver import parse
+
+from mi.dataset.dataset_driver import ParticleDataHandler
+
+
+class DriverTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_one(self):
+
+        sourceFilePath = os.path.join('mi','dataset','driver','cg_dcl_eng','dcl','resource',
+                                      '20140915.syslog.log')
+
+        particle_data_hdlr_obj = ParticleDataHandler()
+
+        particle_data_hdlr_obj = parse(Config().base_dir(), sourceFilePath, particle_data_hdlr_obj)
+
+        log.debug("SAMPLES: %s", particle_data_hdlr_obj._samples)
+        log.debug("FAILURE: %s", particle_data_hdlr_obj._failure)
+
+        self.assertEquals(particle_data_hdlr_obj._failure, False)
+
+
+if __name__ == '__main__':
+    test = DriverTest('test_one')
+    test.test_one()
