9ebd44b6da09450da78645af131708393c686245
Joe Padula <Joseph_J_Padula@raytheon.com>
2014-12-17 13:26:21 -0500

    nutnr_j_cspp drivers and parser update Change-Id: I0fa15a10cdc3e8c4baf574f830ca28af81d4ac48

---
+++ mi/dataset/driver/nutnr_j/cspp/nutnr_j_cspp_telemetered_driver.py
@@ -0,0 +1,62 @@
+"""
+@package mi.dataset.driver.nutnr_j.cspp
+@file mi-dataset/mi/dataset/driver/nutnr_j/cspp/nutnr_j_cspp_telemetered_driver.py
+@author Joe Padula
+@brief Telemetered driver for the nutnr_j_cspp instrument
+
+Release notes:
+
+Initial Release
+"""
+
+__author__ = 'jpadula'
+
+from mi.dataset.dataset_driver import SimpleDatasetDriver
+from mi.dataset.dataset_parser import DataSetDriverConfigKeys
+
+from mi.dataset.parser.cspp_base import \
+    DATA_PARTICLE_CLASS_KEY, \
+    METADATA_PARTICLE_CLASS_KEY
+from mi.dataset.parser.nutnr_j_cspp import \
+    NutnrJCsppMetadataTelemeteredDataParticle, \
+    NutnrJCsppTelemeteredDataParticle, \
+    NutnrJCsppParser
+
+
+def parse(basePythonCodePath, sourceFilePath, particleDataHdlrObj):
+    """
+    This is the method called by Uframe
+    :param basePythonCodePath This is the file system location of mi-dataset
+    :param sourceFilePath This is the full path and filename of the file to be parsed
+    :param particleDataHdlrObj Java Object to consume the output of the parser
+    :return particleDataHdlrObj
+    """
+
+    with open(sourceFilePath, 'r') as stream_handle:
+
+        # create an instance of the concrete driver class defined below
+        driver = NutnrJCsppTelemeteredDriver(basePythonCodePath, stream_handle, particleDataHdlrObj)
+
+        driver.processFileStream()
+
+    return particleDataHdlrObj
+
+
+class NutnrJCsppTelemeteredDriver(SimpleDatasetDriver):
+    """
+    The nutnr_j_cspp telemetered driver class extends the SimpleDatasetDriver.
+    """
+
+    def _build_parser(self, stream_handle):
+
+        parser_config = {
+            DataSetDriverConfigKeys.PARTICLE_CLASSES_DICT: {
+                METADATA_PARTICLE_CLASS_KEY: NutnrJCsppMetadataTelemeteredDataParticle,
+                DATA_PARTICLE_CLASS_KEY: NutnrJCsppTelemeteredDataParticle
+            }
+        }
+
+        parser = NutnrJCsppParser(parser_config,
+                                  stream_handle,
+                                  self._exception_callback)
+        return parser
--- mi/dataset/parser/nutnr_j_cspp.py
+++ mi/dataset/parser/nutnr_j_cspp.py
@@ -1,7 +1,5 @@
-#!/usr/bin/env python
-
 """
-@package mi.dataset.parser.nutnr_j_cspp
+@package mi.dataset.parser
 @file marine-integrations/mi/dataset/parser/nutnr_j_cspp.py
 @author Emily Hahn
 @brief Parser for the nutnr_j_cspp dataset driver
@@ -13,23 +11,22 @@ initial release
 __author__ = 'Emily Hahn'
 __license__ = 'Apache 2.0'
 
-import copy
 import re
-import ntplib
 
-from mi.core.log import get_logger ; log = get_logger()
+from mi.core.log import get_logger
+log = get_logger()
 from mi.core.common import BaseEnum
-from mi.core.instrument.data_particle import DataParticle, DataParticleKey
+from mi.core.instrument.data_particle import DataParticle
 from mi.core.exceptions import RecoverableSampleException
 
-from mi.dataset.dataset_parser import BufferLoadingParser
+from mi.dataset.parser.common_regexes import \
+    END_OF_LINE_REGEX, \
+    INT_REGEX
+
 from mi.dataset.parser.cspp_base import CsppParser, CsppMetadataDataParticle, \
-                                        MetadataRawDataKey, \
-                                        FLOAT_REGEX, INT_REGEX, \
-                                        Y_OR_N_REGEX, \
-                                        END_OF_LINE_REGEX, \
-                                        encode_y_or_n, \
-                                        SAMPLE_START_REGEX
+    encode_y_or_n, \
+    MetadataRawDataKey, \
+    Y_OR_N_REGEX
 
 FLOAT_REGEX_NON_CAPTURE = r'[+-]?[0-9]*\.[0-9]+'
 FLOAT_TAB_REGEX = FLOAT_REGEX_NON_CAPTURE + '\t'
@@ -38,16 +35,16 @@ FLOAT_TAB_REGEX = FLOAT_REGEX_NON_CAPTURE + '\t'
 # use groups to match repeated regexes, split by tabs to get parameter values instead
 LINE_START_REGEX = FLOAT_TAB_REGEX + FLOAT_TAB_REGEX + Y_OR_N_REGEX + '\t'
 DATA_REGEX = LINE_START_REGEX
-DATA_REGEX += '[a-zA-Z]+\t' # Frame Type
-DATA_REGEX += '\d+\t\d+\t' # year, day of year
-DATA_REGEX += '(' + FLOAT_TAB_REGEX + '){6}' # match 6 floats separated by tabs
-DATA_REGEX += '(?:\d+\t){259}' # match 259 ints separated by tabs, non capturing group due to number of groups
-DATA_REGEX += '(' + FLOAT_TAB_REGEX + '){3}' # match 3 floats separated by tabs
-DATA_REGEX += '(' + INT_REGEX + ')\t' # lamp time
-DATA_REGEX += '(' + FLOAT_TAB_REGEX + '){10}' # match 10 floats separated by tabs
-DATA_REGEX += '(\d+)\t' # ctd time
-DATA_REGEX += '(' + FLOAT_TAB_REGEX + '){3}' # match 3 floats separated by tabs
-DATA_REGEX += '[0-9a-fA-F]{2}' + END_OF_LINE_REGEX # checksum and line end
+DATA_REGEX += '[a-zA-Z]+\t'     # Frame Type
+DATA_REGEX += '\d+\t\d+\t'      # year, day of year
+DATA_REGEX += '(' + FLOAT_TAB_REGEX + '){6}'    # match 6 floats separated by tabs
+DATA_REGEX += '(?:\d+\t){259}'  # match 259 ints separated by tabs, non capturing group due to number of groups
+DATA_REGEX += '(' + FLOAT_TAB_REGEX + '){3}'    # match 3 floats separated by tabs
+DATA_REGEX += '(' + INT_REGEX + ')\t'   # lamp time
+DATA_REGEX += '(' + FLOAT_TAB_REGEX + '){10}'   # match 10 floats separated by tabs
+DATA_REGEX += '(\d+)\t'         # ctd time
+DATA_REGEX += '(' + FLOAT_TAB_REGEX + '){3}'    # match 3 floats separated by tabs
+DATA_REGEX += '[0-9a-fA-F]{2}' + END_OF_LINE_REGEX  # checksum and line end
 
 NUMBER_CHANNELS = 256
 NUM_FIELDS = 33 + NUMBER_CHANNELS
@@ -119,23 +116,18 @@ class NutnrJCsppMetadataDataParticle(CsppMetadataDataParticle):
         with the appropriate tag.
         @throws RecoverableSampleException If there is a problem with sample creation
         """
-        try:
-            # this particle only contains common metdata values
-            results = self._build_metadata_parsed_values()
-            
-            data_match = self.raw_data[MetadataRawDataKey.DATA_MATCH]
 
-            # split raw data match by tabs to be able to isolate profiler timestamp
-            params = data_match.group(0).split('\t')
+        # this particle only contains common metadata values
+        results = self._build_metadata_parsed_values()
 
-            internal_timestamp_unix = float(params[GRP_PROFILER_TIMESTAMP])
-            self.set_internal_timestamp(unix_time=internal_timestamp_unix)
+        data_match = self.raw_data[MetadataRawDataKey.DATA_MATCH]
 
-        except (ValueError, TypeError, IndexError) as ex:
-            log.warn("Error %s while building parsed values from data %s", ex, self.raw_data)
-            raise RecoverableSampleException("Error %s while building parsed values from data %s" %
-                                             (ex, self.raw_data))
-        
+        # split raw data match by tabs to be able to isolate profiler timestamp
+        params = data_match.group(0).split('\t')
+
+        internal_timestamp_unix = float(params[GRP_PROFILER_TIMESTAMP])
+        self.set_internal_timestamp(unix_time=internal_timestamp_unix)
+
         return results
 
 
@@ -162,32 +154,27 @@ class NutnrJCsppDataParticle(DataParticle):
         @throws RecoverableSampleException If there is a problem with sample creation
         """
         results = []
-        try:
-            # split the entire matched line by tabs, which will return each parameters
-            # value as an array of string
-            params = self.raw_data.group(0).split('\t')
-            if len(params) < NUM_FIELDS:
-                log.warn('Not enough fields could be parsed from the data %s', 
-                         self.raw_data.group(0))
-                raise RecoverableSampleException('Not enough fields could be parsed from the data %s' %
-                                                 self.raw_data.group(0))
 
-            for name, index, encode_function in PARAMETER_MAP:
-                if name == SPECTRAL_CHANNELS:
-                    # spectral channels is an array of ints, need to do the extra map 
-                    results.append(self._encode_value(name,
-                                                      map(int, params[index:GRP_SPECTRAL_END]),
-                                                      encode_function))
-                else:
-                    results.append(self._encode_value(name, params[index], encode_function))
+        # split the entire matched line by tabs, which will return each parameters
+        # value as an array of string
+        params = self.raw_data.group(0).split('\t')
+        if len(params) < NUM_FIELDS:
+            log.warn('Not enough fields could be parsed from the data %s',
+                     self.raw_data.group(0))
+            raise RecoverableSampleException('Not enough fields could be parsed from the data %s' %
+                                             self.raw_data.group(0))
 
-            internal_timestamp_unix = float(params[GRP_PROFILER_TIMESTAMP])
-            self.set_internal_timestamp(unix_time=internal_timestamp_unix)
+        for name, index, encode_function in PARAMETER_MAP:
+            if name == SPECTRAL_CHANNELS:
+                # spectral channels is an array of ints, need to do the extra map
+                results.append(self._encode_value(name,
+                                                  map(int, params[index:GRP_SPECTRAL_END]),
+                                                  encode_function))
+            else:
+                results.append(self._encode_value(name, params[index], encode_function))
 
-        except (ValueError, TypeError, IndexError) as ex:
-            log.warn("Error %s while building parsed values from data %s", ex, self.raw_data)
-            raise RecoverableSampleException("Error %s while building parsed values from data %s" %
-                                             (ex, self.raw_data))
+        internal_timestamp_unix = float(params[GRP_PROFILER_TIMESTAMP])
+        self.set_internal_timestamp(unix_time=internal_timestamp_unix)
 
         return results
 
@@ -206,25 +193,17 @@ class NutnrJCsppParser(CsppParser):
 
     def __init__(self,
                  config,
-                 state,
                  stream_handle,
-                 state_callback,
-                 publish_callback,
-                 exception_callback,
-                 *args, **kwargs):
+                 exception_callback):
         """
-        This the contstructor which instantiates the NutnrJCsppParser
+        This the constructor which instantiates the NutnrJCsppParser
         """
     
         # call the superclass constructor
         super(NutnrJCsppParser, self).__init__(config,
-                                               state,
                                                stream_handle,
-                                               state_callback,
-                                               publish_callback,
                                                exception_callback,
                                                DATA_REGEX,
-                                               ignore_matcher=IGNORE_MATCHER,
-                                               *args, **kwargs)
+                                               ignore_matcher=IGNORE_MATCHER)
 
 
