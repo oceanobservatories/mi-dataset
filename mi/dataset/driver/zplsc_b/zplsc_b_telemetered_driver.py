b62ac388d865a6a5b47e4c3216e623c7474245fc
ronkyo <ronkyo@gmail.com>
2014-12-18 14:16:27 -0800

    Initial code for zplsc-b.
    Change-Id: I50d2c1f64e16509afb8886cced7e7a622b4f4395

---
+++ mi/dataset/driver/zplsc_b/zplsc_b_telemetered_driver.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+"""
+@package mi.dataset.driver.zplsc_b
+@file mi-dataset/mi/dataset/driver/zplsc_b/zplsc_b_telemetered_driver.py
+@author Ronald Ronquillo
+@brief Recovered driver for the zplsc_b instrument
+
+Release notes:
+
+Initial Release
+"""
+
+from mi.dataset.dataset_parser import DataSetDriverConfigKeys
+from mi.dataset.dataset_driver import SimpleDatasetDriver
+from mi.dataset.parser.zplsc_b import ZplscBParser
+
+
+def parse(basePythonCodePath, sourceFilePath, outputFilePath, particleDataHdlrObj):
+    """
+    This is the method called by Uframe
+    :param basePythonCodePath This is the file system location of mi-dataset
+    :param sourceFilePath This is the full path and filename of the file to be parsed
+    :param particleDataHdlrObj Java Object to consume the output of the parser
+    :return particleDataHdlrObj
+    """
+
+    with open(sourceFilePath, 'rb') as stream_handle:
+
+        ZplscBTelemeteredDriver(basePythonCodePath, stream_handle, outputFilePath,
+                                particleDataHdlrObj).processFileStream()
+
+    return particleDataHdlrObj
+
+
+class ZplscBTelemeteredDriver(SimpleDatasetDriver):
+    """
+    The zplsc_b driver class extends the SimpleDatasetDriver.
+    """
+
+    def __init__(self, basePythonCodePath, stream_handle, outputFilePath, particleDataHdlrObj):
+
+        self.outputFilePath = outputFilePath
+
+        super(ZplscBTelemeteredDriver, self).__init__(basePythonCodePath, stream_handle, particleDataHdlrObj)
+
+    def _build_parser(self, stream_handle):
+
+        parser_config = {
+            DataSetDriverConfigKeys.PARTICLE_MODULE: 'mi.dataset.parser.zplsc_b',
+            DataSetDriverConfigKeys.PARTICLE_CLASS: 'ZplscBInstrumentDataParticle'}
+
+        parser = ZplscBParser(parser_config,
+                              stream_handle,
+                              self._exception_callback,
+                              self.outputFilePath)
+
+        return parser
