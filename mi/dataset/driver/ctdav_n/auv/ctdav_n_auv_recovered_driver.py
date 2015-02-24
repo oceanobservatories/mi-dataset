65f0824cd279f229feb3c81879c54e95fb5178fa
Jeffrey Roy <Jeffrey_A_Roy@Raytheon.com>
2015-02-24 16:05:03 -0500

    ctdav parser and driver all testing compelte
    Change-Id: Idc27486cfd35ad0360d41ecab935061cdc545814

---
+++ mi/dataset/driver/ctdav_n/auv/ctdav_n_auv_recovered_driver.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+"""
+@package mi.dataset.driver.ctdav_n.auv
+@file mi/dataset/driver/ctdav_n/auv/ctdav_n_auv_recovered_driver.py
+@author Jeff Roy
+@brief Driver for the ctdav_n_auv instrument
+
+Release notes:
+
+Initial Release
+"""
+
+from mi.dataset.dataset_driver import SimpleDatasetDriver
+from mi.dataset.parser.ctdav_n_auv import CtdavNAuvParser
+
+
+def parse(basePythonCodePath, sourceFilePath, particleDataHdlrObj):
+    """
+    This is the method called by Uframe
+    :param basePythonCodePath This is the file system location of mi-dataset
+    :param sourceFilePath This is the full path and filename of the file to be parsed
+    :param particleDataHdlrObj Java Object to consume the output of the parser
+    :return particleDataHdlrObj
+    """
+
+    with open(sourceFilePath, 'rU') as stream_handle:
+
+        # create and instance of the concrete driver class defined below
+        driver = CtdavNAuvRecoveredDriver(basePythonCodePath, stream_handle, particleDataHdlrObj)
+        driver.processFileStream()
+
+    return particleDataHdlrObj
+
+
+class CtdavNAuvRecoveredDriver(SimpleDatasetDriver):
+    """
+    Derived adcpa_n_auv driver class
+    All this needs to do is create a concrete _build_parser method
+    """
+
+    def _build_parser(self, stream_handle):
+
+        parser = CtdavNAuvParser(stream_handle,
+                                 self._exception_callback,
+                                 is_telemetered=False)
+
+        return parser
+
+
