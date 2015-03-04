9c57fe498da813900076487e1d6061f198abbc30
Jeffrey Roy <Jeffrey_A_Roy@Raytheon.com>
2015-03-04 10:37:10 -0500

    auv_eng_auv parser unit tested auv_eng_drivers drivers have not been validated, need to re-establish Edex environment
    Change-Id: I1911e49e13d18efd98cf3fbeec8e4f424894094a

---
+++ mi/dataset/driver/auv_eng/auv/auv_eng_auv_recovered_driver.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+"""
+@package mi.dataset.driver.auv_eng.auv
+@file mi/dataset/driver/auv_eng/auv/auv_eng_auv_recovered_driver.py
+@author Jeff Roy
+@brief Driver for the auv_eng_auv instrument
+
+Release notes:
+
+Initial Release
+"""
+
+from mi.dataset.dataset_driver import SimpleDatasetDriver
+from mi.dataset.parser.auv_eng_auv import AuvEngAuvParser
+
+
+def parse(basePythonCodePath, sourceFilePath, particleDataHdlrObj):
+    """
+    This is the method called by Uframe
+    :param basePythonCodePath This is the file system location of mi-dataset
+    :param sourceFilePath This is the full path and filename of the file to be parsed
+    :param particleDataHdlrObj Java Object to consume the output of the parser
+    :return particleDataHdlrObj
+    """
+
+    with open(sourceFilePath, 'rU') as stream_handle:
+
+        # create and instance of the concrete driver class defined below
+        driver = AuvEngAuvRecoveredDriver(basePythonCodePath, stream_handle, particleDataHdlrObj)
+        driver.processFileStream()
+
+    return particleDataHdlrObj
+
+
+class AuvEngAuvRecoveredDriver(SimpleDatasetDriver):
+    """
+    Derived auv_eng_auv driver class
+    All this needs to do is create a concrete _build_parser method
+    """
+
+    def _build_parser(self, stream_handle):
+
+        parser = AuvEngAuvParser(stream_handle,
+                                 self._exception_callback,
+                                 is_telemetered=False)
+
+        return parser
+
+
