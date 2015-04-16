93ce7334c00cf1e39410e58277118405fb5f83a8
Benjamin Doan <Benjamin.Doan@raytheon.com>
2015-04-16 10:25:34 -0500

    added setup.py
    Required for installing mi-dataset with pip
    
    Change-Id: Ifbfde1255ec2ac7e03b332c48696b409cbc6bcb3

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mi-dataset",
+    version="0.0.1",
+    description="Contains marine integrations dataset drivers and parsers",
+    url="http://github.com/oceanobservatories/mi-dataset",
+    license="BSD",
+    py_modules = ['mi']
+)
