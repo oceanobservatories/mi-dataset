65f03cd1bbbe7f99bf61eeb9f449d0c152419527
Jeffrey Roy <Jeffrey_A_Roy@Raytheon.com>
2015-04-29 15:23:10 -0400

    Updates for Issue #3223 (child of #3021)
    IDD and code both needed updating to work with live files.
    Requires updates to streams.xml
    Parameter was dropped from adcpt_acfgm_pd8_dcl_instrument and adcpt_acfgm_pd8_dcl_instrument_recovered

--- mi/dataset/parser/adcpt_acfgm_dcl_pd8.py
+++ mi/dataset/parser/adcpt_acfgm_dcl_pd8.py
@@ -18,7 +18,6 @@ Mal-formed sensor data records and all dcl log records produce no particles.
 
 The sensor data record has the following format:
 
-<DCL Timestamp First> [<IDENTIFIER>]:Instrument Started [Power On]
 <DCL Timestamp> <Instrument Timestamp> <Ensemble Number>
 <DCL Timestamp> Hdg: <Heading> Pitch: <Pitch> Roll: <Roll>
 <DCL Timestamp> Temp: <Temperature> SoS: <Speed of Sound> BIT: <BIT>
@@ -26,9 +25,6 @@ The sensor data record has the following format:
 <DCL Timestamp>   1   <DIR 1>   <MAG 1>      <EW 1>      <NS 1>      <VERT 1>       <ERR 1>    <ECHO1 1>    <ECHO2 1>    <ECHO3 1>    <ECHO4 1>
 <DCL Timestamp>   ...
 <DCL Timestamp>   <N>   <DIR N>   <MAG N>      <EW N>      <NS N>      <VERT N>       <ERR N>    <ECHO1 N>    <ECHO2 N>    <ECHO3 N>    <ECHO4 N>
-<DCL Timestamp>
-<DCL Timestamp> [<IDENTIFIER>]:Instrument Stopped [Power Off].
-<DCL Timestamp Last> [<IDENTIFIER>]:
 
 
 Release notes:
@@ -51,13 +47,14 @@ from mi.dataset.parser.dcl_file_common import DclInstrumentDataParticle, \
 from mi.core.exceptions import RecoverableSampleException
 from mi.dataset.dataset_parser import SimpleParser
 
-from mi.dataset.parser.common_regexes import FLOAT_REGEX, UNSIGNED_INT_REGEX, INT_REGEX, SPACE_REGEX, ANY_CHARS_REGEX, \
-    ASCII_HEX_CHAR_REGEX
+from mi.dataset.parser.common_regexes import FLOAT_REGEX, UNSIGNED_INT_REGEX, INT_REGEX, \
+    SPACE_REGEX, ANY_CHARS_REGEX, ASCII_HEX_CHAR_REGEX
 
 # Basic patterns
 UINT = '(' + UNSIGNED_INT_REGEX + ')'   # unsigned integer as a group
 SINT = '(' + INT_REGEX + ')'            # signed integer as a group
 FLOAT = '(' + FLOAT_REGEX + ')'         # floating point as a captured group
+FLOAT_OR_DASH = '(' + FLOAT_REGEX + '|\-\-' + ')'         # floating point as a captured group
 MULTI_SPACE = SPACE_REGEX + '+'
 ANY_NON_BRACKET_CHAR = r'[^\[\]]+'
 
@@ -74,24 +71,6 @@ DCL_LOG_PATTERN += ANY_NON_BRACKET_CHAR     # followed by text
 DCL_LOG_PATTERN += END_METADATA             # followed by ']'
 DCL_LOG_PATTERN += ANY_CHARS_REGEX          # followed by more text
 DCL_LOG_MATCHER = re.compile(DCL_LOG_PATTERN)
-
-# DCL Power On Log record:
-# 2013/12/01 01:00:20.797 [adcp:DLOGP1]:Instrument Started [Power On]\n
-DCL_LOG_ON_PATTERN = TIMESTAMP + SPACE_REGEX                # DCL controller timestamp
-DCL_LOG_ON_PATTERN += START_METADATA                        # Metadata record starts with '['
-DCL_LOG_ON_PATTERN += ANY_NON_BRACKET_CHAR                  # followed by text
-DCL_LOG_ON_PATTERN += END_METADATA                          # followed by ']'
-DCL_LOG_ON_PATTERN += '.*? \[Power On\]'    # followed by power on text
-DCL_LOG_ON_MATCHER = re.compile(DCL_LOG_ON_PATTERN)
-
-# DCL Power Off Log record:
-# 2013/12/01 01:06:08.038 [adcp:DLOGP1]:Instrument Stopped [Power Off]\n
-DCL_LOG_OFF_PATTERN = TIMESTAMP + SPACE_REGEX               # DCL controller timestamp
-DCL_LOG_OFF_PATTERN += START_METADATA                       # Metadata record starts with '['
-DCL_LOG_OFF_PATTERN += ANY_NON_BRACKET_CHAR                 # followed by text
-DCL_LOG_OFF_PATTERN += END_METADATA                         # followed by ']'
-DCL_LOG_OFF_PATTERN += '.*?Power Off'  # followed by power off text
-DCL_LOG_OFF_MATCHER = re.compile(DCL_LOG_OFF_PATTERN)
 
 # Header 1
 # 2013/12/01 01:04:18.213 2013/12/01 01:00:27.53 00001\n
@@ -127,8 +106,8 @@ IGNORE_HEADING_MATCHER = re.compile(IGNORE_HEADING_PATTERN)
 # 2013/12/01 01:04:18.446   1   14.6   47.5      12      46      -3       5    162    168    164    168\n
 SENSOR_DATA_PATTERN = TIMESTAMP + MULTI_SPACE   # DCL controller timestamp
 SENSOR_DATA_PATTERN += UINT + MULTI_SPACE       # bin
-SENSOR_DATA_PATTERN += FLOAT + MULTI_SPACE      # Dir
-SENSOR_DATA_PATTERN += FLOAT + MULTI_SPACE      # Mag
+SENSOR_DATA_PATTERN += FLOAT_OR_DASH + MULTI_SPACE      # Dir
+SENSOR_DATA_PATTERN += FLOAT_OR_DASH + MULTI_SPACE      # Mag
 SENSOR_DATA_PATTERN += SINT + MULTI_SPACE       # E/W
 SENSOR_DATA_PATTERN += SINT + MULTI_SPACE       # N/S
 SENSOR_DATA_PATTERN += SINT + MULTI_SPACE       # Vert
@@ -166,32 +145,42 @@ TEMP_HEX = 10
 # SENSOR_DATA_MATCHER produces the following groups.
 SENSOR_DATA_BIN = 8
 
+
+#  encoding function for water_velocity and water_direction
+#  the adcp puts "--" in the output when these can't be computed
+#  return fill value when found
+def float_or_dash(val):
+    if val == '--':
+        return None
+    else:
+        return float(val)
+
+
 # The following is used for DclInstrumentDataParticle._build_parsed_values() and defined as below:
 # (parameter name, index into parsed_data[], encoding function)
 PD8_DATA_MAP = [
     ('dcl_controller_timestamp',            0,  str),       # Last timestamp from the DCL controller
-    ('dcl_controller_starting_timestamp',   8,  str),
-    ('instrument_timestamp',                9,  str),
-    ('ensemble_number',                     10, int),
-    ('heading',                             11, float),
-    ('pitch',                               12, float),
-    ('roll',                                13, float),
-    ('temperature',                         14, float),
-    ('speed_of_sound',                      15, int),
-    ('bit_result_demod_1',                  16, int),
-    ('bit_result_demod_0',                  17, int),
-    ('bit_result_timing',                   18, int),
-    ('water_direction',                     19, lambda x: [float(y) for y in x]),
-    ('water_velocity',                      20, lambda x: [float(y) for y in x]),
-    ('water_velocity_east',                 21, lambda x: [int(y) for y in x]),
-    ('water_velocity_north',                22, lambda x: [int(y) for y in x]),
-    ('water_velocity_up',                   23, lambda x: [int(y) for y in x]),
-    ('error_velocity',                      24, lambda x: [int(y) for y in x]),
-    ('echo_intensity_beam1',                25, lambda x: [int(y) for y in x]),
-    ('echo_intensity_beam2',                26, lambda x: [int(y) for y in x]),
-    ('echo_intensity_beam3',                27, lambda x: [int(y) for y in x]),
-    ('echo_intensity_beam4',                28, lambda x: [int(y) for y in x]),
-    ('num_cells',                           29, int)
+    ('instrument_timestamp',                8,  str),
+    ('ensemble_number',                     9, int),
+    ('heading',                             10, float),
+    ('pitch',                               11, float),
+    ('roll',                                12, float),
+    ('temperature',                         13, float),
+    ('speed_of_sound',                      14, int),
+    ('bit_result_demod_1',                  15, int),
+    ('bit_result_demod_0',                  16, int),
+    ('bit_result_timing',                   17, int),
+    ('water_direction',                     18, lambda x: [float_or_dash(y) for y in x]),
+    ('water_velocity',                      19, lambda x: [float_or_dash(y) for y in x]),
+    ('water_velocity_east',                 20, lambda x: [int(y) for y in x]),
+    ('water_velocity_north',                21, lambda x: [int(y) for y in x]),
+    ('water_velocity_up',                   22, lambda x: [int(y) for y in x]),
+    ('error_velocity',                      23, lambda x: [int(y) for y in x]),
+    ('echo_intensity_beam1',                24, lambda x: [int(y) for y in x]),
+    ('echo_intensity_beam2',                25, lambda x: [int(y) for y in x]),
+    ('echo_intensity_beam3',                26, lambda x: [int(y) for y in x]),
+    ('echo_intensity_beam4',                27, lambda x: [int(y) for y in x]),
+    ('num_cells',                           28, int)
 ]
 
 
@@ -239,42 +228,40 @@ class AdcptAcfgmPd8Parser(SimpleParser):
         this method self._record_buffer will be filled with all the particles in the file.
         """
 
-        while True:
-            line = self._stream_handle.readline()
+        while True:  # loop through file looking for beginning of an adcp data burst
+
+            line = self._stream_handle.readline()  # READ NEXT LINE
+
             if line == "":
                 break
 
-            # Check if this is the power on log
-            dcl_log_match = DCL_LOG_ON_MATCHER.match(line)
-            if dcl_log_match is None:
-                # Otherwise, this could be a regular DCL Log
-                dcl_log_match = DCL_LOG_MATCHER.match(line)
-                if dcl_log_match is None:
-                    self.recov_exception_callback('Unknown data found in line %s' % line)
-                # Else verified to be a regular DCL Log. Discard & move to next line.
-                continue
-
-            # First timestamp from the DCL controller log
-            parsed_data = [dcl_log_match.groups()[SENSOR_GROUP_TIMESTAMP], ]
-            line = self._stream_handle.readline()
+            # Check if this is a DCL Log message
+            dcl_log_match = DCL_LOG_MATCHER.match(line)
+            if dcl_log_match:
+                # verified to be a regular DCL Log. Discard & move to next line.
+                continue  # skip to next line in outer loop
 
             line_match = SENSOR_TIME_MATCHER.match(line)
             if line_match is None:
                 self.recov_exception_callback("Expected starting DCL Timestamp, received: %s" % line)
-                continue
+                continue  # skip to next line in outer loop
 
             matches = line_match.groups()
             sensor_data_list = []
+
+            # Save timestamp from the DCL controller log and it's parts
+            parsed_data = list(matches[SENSOR_GROUP_TIMESTAMP:SENSOR_TIME_SENSOR_DATE_TIME])
 
             # Get instrument_timestamp & ensemble_number
             parsed_data.append(matches[SENSOR_TIME_SENSOR_DATE_TIME])
             parsed_data.append(matches[SENSOR_TIME_ENSEMBLE])
 
-            line = self._stream_handle.readline()
+            line = self._stream_handle.readline()  # READ NEXT LINE
+
             line_match = SENSOR_HEAD_MATCHER.match(line)
             if line_match is None:
                 self.recov_exception_callback("Expecting Heading, Pitch, & Roll data, received: %s" % line)
-                continue
+                continue  # skip to next line in outer loop
 
             matches = line_match.groups()
             # Get head, pitch, & roll
@@ -282,12 +269,13 @@ class AdcptAcfgmPd8Parser(SimpleParser):
             parsed_data.append(matches[HEAD_PITCH])
             parsed_data.append(matches[HEAD_ROLL])
 
-            line = self._stream_handle.readline()
+            line = self._stream_handle.readline()  # READ NEXT LINE
+
             line_match = SENSOR_TEMP_MATCHER.match(line)
             if line_match is None:
                 self.recov_exception_callback("Expecting Temperature, Speed of Sound, & BIT data,"
                                               " received: %s" % line)
-                continue
+                continue  # skip to next line in outer loop
 
             matches = line_match.groups()
             # Get temperature,  speed of sound, & BIT values
@@ -299,49 +287,39 @@ class AdcptAcfgmPd8Parser(SimpleParser):
             parsed_data.append(binary_string[4])
             parsed_data.append(binary_string[6])
 
-            line = self._stream_handle.readline()
+            line = self._stream_handle.readline()  # READ NEXT LINE
+
             line_match = IGNORE_HEADING_MATCHER.match(line)
             if line_match is None:
                 self.recov_exception_callback("Expecting Header, received: %s" % line)
-                continue
+                continue  # skip to next line in outer loop
 
             # Start looking for sensor data
-            while True:
-                line = self._stream_handle.readline()
+            while True:  # loop through all the velocity and echo data records
+
+                line = self._stream_handle.readline()  # READ NEXT LINE
+
                 line_match = SENSOR_DATA_MATCHER.match(line)
                 if line_match is not None:
                     # Collect velocity data sextets and echo power quartets
                     sensor_data_list.append(line_match.groups()[SENSOR_DATA_BIN:])
-
                 else:
-                    line_match = IGNORE_EMPTY_MATCHER.match(line)
-                    if line_match is None:
-                        self.recov_exception_callback("Expecting empty DCL Timestamp, received: %r" % line)
-                        break
+                    try:
+                        # Transpose velocity data sextets and echo power quartets
+                        np_array = numpy.array(sensor_data_list)
+                        parsed_data.extend(np_array.transpose().tolist()[1:])
 
-                    line = self._stream_handle.readline()
-                    dcl_log_match = DCL_LOG_OFF_MATCHER.match(line)
-                    if dcl_log_match is None:
-                        self.recov_exception_callback("Expecting Off DCL Timestamp, received: %r" % line)
-                        break
+                        # Get number of cells
+                        parsed_data.append(sensor_data_list[-1][0])
+                        particle = self._extract_sample(self._particle_class,
+                                                        None,
+                                                        parsed_data,
+                                                        None)
+                        if particle is not None:
+                            self._record_buffer.append(particle)
 
-                    particle_data = []
-                    # Last timestamp from the DCL controller log
-                    particle_data[:0] = dcl_log_match.groups()
-                    particle_data.extend(parsed_data)
+                    except Exception:
+                        self.recov_exception_callback("Error parsing sensor data row,"
+                                                      " received: %s" % line)
 
-                    # Transpose velocity data sextets and echo power quartets
-                    np_array = numpy.array(sensor_data_list)
-                    particle_data.extend(np_array.transpose().tolist()[1:])
-
-                    # Get number of cells
-                    particle_data.append(sensor_data_list[-1][0])
-
-                    particle = self._extract_sample(self._particle_class,
-                                                    None,
-                                                    particle_data,
-                                                    None)
-                    if particle is not None:
-                        self._record_buffer.append(particle)
-
-                    break
+                    break  # exit inner loop once a particle has been produced
