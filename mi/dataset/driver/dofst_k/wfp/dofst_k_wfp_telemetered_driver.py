6823fdf884d8585e6e1cbc8668c2b4694bc20753
Jeffrey Roy <Jeffrey_A_Roy@Raytheon.com>
2015-06-24 10:57:21 -0400

    Refactored dofst_k_wfp drivers to newer design pattern/standards

--- mi/dataset/driver/dofst_k/wfp/dofst_k_wfp_telemetered_driver.py
+++ mi/dataset/driver/dofst_k/wfp/dofst_k_wfp_telemetered_driver.py
@@ -4,36 +4,60 @@
 #
 # Copyright 2014 Raytheon Co.
 ##
-__author__ = 'kustert'
+__author__ = 'jroy'
 
 import os
-import sys
 
 from mi.dataset.dataset_parser import DataSetDriverConfigKeys
-from mi.dataset.driver.dofst_k.wfp.dofst_k_wfp_driver import DofstKWfpDriver
+
+from mi.dataset.dataset_driver import SimpleDatasetDriver
+from mi.dataset.parser.dofst_k_wfp import DofstKWfpParser
 from mi.dataset.parser.dofst_k_wfp_particles import DofstKWfpTelemeteredDataParticle, \
     DofstKWfpTelemeteredMetadataParticle
 
+
 def parse(basePythonCodePath, sourceFilePath, particleDataHdlrObj):
-    
-    try:
-        if basePythonCodePath is not None:
-            pass
-    except NameError:
-        basePythonCodePath = os.curdir
-    sys.path.append(basePythonCodePath)
-    
-    from mi.logging import config
-    config.add_configuration(os.path.join(basePythonCodePath, 'res', 'config', 'mi-logging.yml'))
-    
-    config = {
-        DataSetDriverConfigKeys.PARTICLE_MODULE: 'mi.dataset.parser.dofs_k_wfp_particles',
-        DataSetDriverConfigKeys.PARTICLE_CLASS: None,
-        DataSetDriverConfigKeys.PARTICLE_CLASSES_DICT: {
-            'instrument_data_particle_class': DofstKWfpTelemeteredDataParticle,
-            'metadata_particle_class': DofstKWfpTelemeteredMetadataParticle
+    """
+    This is the method called by Uframe
+    :param basePythonCodePath This is the file system location of mi-dataset
+    :param sourceFilePath This is the full path and filename of the file to be parsed
+    :param particleDataHdlrObj Java Object to consume the output of the parser
+    :return particleDataHdlrObj
+    """
+
+    with open(sourceFilePath, 'rb') as stream_handle:
+
+        # create and instance of the concrete driver class defined below
+        driver = DofstKWfpTelemeteredDriver(basePythonCodePath, stream_handle, particleDataHdlrObj)
+        driver.processFileStream()
+
+    return particleDataHdlrObj
+
+
+class DofstKWfpTelemeteredDriver(SimpleDatasetDriver):
+    """
+    Derived dofst_k_wfp driver class
+    All this needs to do is create a concrete _build_parser method
+    """
+
+    def _build_parser(self, stream_handle):
+
+        filesize = os.path.getsize(stream_handle.name)
+
+        config = {
+            DataSetDriverConfigKeys.PARTICLE_MODULE: 'mi.dataset.parser.dofs_k_wfp_particles',
+            DataSetDriverConfigKeys.PARTICLE_CLASS: None,
+            DataSetDriverConfigKeys.PARTICLE_CLASSES_DICT: {
+                'instrument_data_particle_class': DofstKWfpTelemeteredDataParticle,
+                'metadata_particle_class': DofstKWfpTelemeteredMetadataParticle
+            }
         }
-    }
-    
-    driver = DofstKWfpDriver(basePythonCodePath, sourceFilePath, particleDataHdlrObj, config)
-    return driver.process()
+        parser = DofstKWfpParser(config,
+                                 None,
+                                 stream_handle,
+                                 lambda state, ingested: None,
+                                 lambda data: None,
+                                 self._exception_callback,
+                                 filesize)
+
+        return parser
