4191e59540133ce73fea816bbb4ce85199220598
Jeffrey Roy <Jeffrey_A_Roy@Raytheon.com>
2015-08-11 10:16:32 -0400

    Common parser for the CTDBP series P with attached DOSTA and FLORD Individual Drivers for CTDBP, DOSTA and FLORD Unit tests for all modules
    Change-Id: I86e625dc959b796bb3f9a9fce361edeaad7c446e

---
+++ mi/dataset/driver/ctdbp_p/dcl/test/test_ctdbp_p_dcl_telemetered_driver.py
@@ -0,0 +1,42 @@
+_author__ = 'jeff roy'
+
+from mi.core.log import get_logger
+log = get_logger()
+
+from mi.idk.config import Config
+
+import unittest
+import os
+from mi.dataset.driver.ctdbp_p.dcl.ctdbp_p_dcl_telemetered_driver import parse
+
+from mi.dataset.dataset_driver import ParticleDataHandler
+
+
+class DriverTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_one(self):
+
+        sourceFilePath = os.path.join('mi',
+                                      'dataset', 'driver', 'ctdbp_p',
+                                      'dcl', 'resource',
+                                      'ctdbp01_20150804_061734.DAT')
+
+        particle_data_hdlr_obj = ParticleDataHandler()
+
+        particle_data_hdlr_obj = parse(Config().base_dir(), sourceFilePath, particle_data_hdlr_obj)
+
+        log.debug("SAMPLES: %s", particle_data_hdlr_obj._samples)
+        log.debug("FAILURE: %s", particle_data_hdlr_obj._failure)
+
+        self.assertEquals(particle_data_hdlr_obj._failure, False)
+
+
+if __name__ == '__main__':
+    test = DriverTest('test_one')
+    test.test_one()
